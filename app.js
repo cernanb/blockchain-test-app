c4a2a57b378dbe869bb0954711f6035df342d2c3
Cernan Bernardo <cernanb@gmail.com>
2017-12-27 21:28:24 -0800

    Add web3 to app with endpoint to access accounts

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const expess = require('express')
+const express = require('express')
 
 const app = express()
+const Web3 = require('web3')
+
+const web3 = new Web3('http://localhost:8545')
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`)
+})
+
+app.get('/accounts', (req, res) => {
+  web3.eth.getAccounts().then(accounts => {
+    res.send(accounts)
+  })
+})
+
+app.listen(3000)
